f7915aabfe2847074743bdf9e2f08d8aebc5a1ce
marcus <marcus@linearsky.com>
2024-06-07 17:16:56 +0100

    fix: project typos

--- constants/Projects.js
+++ constants/Projects.js
@@ -5,30 +5,23 @@ const projects= {
 
             {
             name: 'Featurewall',
-            description: <Fragment><p>API based custom website. Allowing users to pick out designs from thousands of images to use as a wallpaper mural. </p><p>Features include; Custom Cropper,  bespoke Calendar and the websites communicates several APIs to facilates the selection of images and to purchase a product.</p></Fragment>,
+            description: <Fragment><p>This API-based, custom Wordpress website enables users to choose from thousands of images to create personalized wallpaper murals. </p><p> Features include: custom Cropper,  bespoke Calendar, multiple APIs for image selection and to facilitate the order for the installation team.</p></Fragment>,
             image: '/images/portfolio-imdecor2.png',
             href:   'https://www.featurewall.co.uk/',
         },
        {
             name:   'TCL Cloud',
-            description: <Fragment><p>Custom web application written in Laravel. That details the whole process of hiring out their fleet of vehiclkes</p><p>Features include: digital signatures, contracts, video recordering and is all fully managed via the backend admin that is styled via Tailwind.</p></Fragment>,
+            description: <Fragment><p>This custom web application, built with Laravel and Vue, streamlines the entire process of hiring out a fleet of vehicles.</p> <p> Features include: digital signatures, contracts, video recordering and is all fully managed via the backend admin that is styled via Tailwind</p></Fragment>,
             image: '/images/portfolio-tcl.jpg',
             href:   'https://tanker.cloud',
         },
         {
             name: 'Follwells',
-            description: <Fragment><p>A custom Wordpress plugin that integrates with Expert Agent API and stores properties in the backend fetched via a cron job. The frontend is a custom javascript theme with filtering and search.</p></Fragment>,
+            description: <Fragment><p>This custom WordPress plugin integrates with the Expert Agent API to fetch and store property listings</p> <p>Features include: custom frontend theme with custom property filtering, plugin to manage listings and automatic scheduler for property imports. </p></Fragment>,
             image: '/images/portfolio-follwells.jpg',
             href:   'https://follwells.co.uk/properties-for-sale/',
         },
         
-      
-        {
-            name: 'Ronbrooks',
-            description: <Fragment><p>A Custom Wordpress application that integrates with the AutoTrader API. Car data is kept uptodate vi a custom API and webhook.</p></Fragment>,
-            image: '/images/portfolio-rb.jpg',
-            href:   'https://www.ronbrooks.co.uk/used-cars/',
-        },
 
     
     ]
