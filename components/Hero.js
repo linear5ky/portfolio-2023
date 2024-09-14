2d14799d62693d5d9f57111e926a3ec2601d1dd3
marcus <marcus@linearsky.com>
2024-09-14 10:09:32 +0100

    chore: update me

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import { useRouter } from "next/router";
-import {motion} from 'framer-motion';
-
+import { motion } from "framer-motion";
 
 export default function Hero() {
   const router = useRouter();
@@ -15,50 +14,46 @@ export default function Hero() {
   }, []);
 
   return (
+    <section class="flex fullheight-mobile-up extraPaddingTopBottom" id="about">
+      <div class="container">
+        <div class="hero">
+          <div class="content">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, ease: "easeOut" }}
+            >
+              <h1>
+                Hi, I&apos;m <span class="name">Marcus</span>
+              </h1>
+              <h2>
+                I am a <span class="bold">freelance full stack developer</span>{" "}
+                based in Staffordshire.
+              </h2>
+              <p>
+                I build <span class="bold">custom website solutions</span> using
+                Wordpress, Laravel Shopify and Next.js.
+              </p>
+            </motion.div>
+          </div>
 
-    
-    <section class="flex fullheight-mobile-up extraPaddingTopBottom"  id="about" >
-            <div class="container">
-    
-                        <div class="hero">
-                            <div class="content"> 
-
-                              <motion.div initial={{ opacity: 0 }}   
-                                          animate={{ opacity: 1}} 
-                                          transition={{duration:1, ease: "easeOut"}}
-                                          
-                                          >
-                                  <h1>Hi, I&apos;m <span class="name">Marcus</span></h1>
-                                  <h2>I am a <span class="bold">freelance full stack developer</span> based in Staffordshire.</h2>
-                                  <p>I build <span class="bold">custom website solutions</span> using Wordpress, Laravel Shopify and Next.js.</p>
-                              </motion.div>
-
-
-                            </div>
-
-
-                             <motion.div 
-                                  initial={{  opacity: 0 }}
-                                  animate={{ x: 0, opacity:1 }}
-                                  transition={{duration:0.5}}>
-                                  <div class="hero-image  circle">
-                      
-                                              <Image
-                                                  src="/images/me2.png"
-                                                  alt="Next.js Logo"
-                                                  width={200}
-                                                  height={200}
-                                                  priority
-                                                />
-                              
-                                  </div>
-                                  </motion.div>
-                        </div>
-                   
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div class="hero-image  circle">
+              <Image
+                src="/images/me3.png"
+                alt="Next.js Logo"
+                width={200}
+                height={200}
+                priority
+              />
             </div>
+          </motion.div>
+        </div>
+      </div>
     </section>
-  
-
-    );
+  );
 }
- 
