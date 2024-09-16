b210f99e8224860b91afb642ae5dcf2afc65b1e9
linear5ky <marcus.powell@linearsky.com>
2024-09-16 14:32:14 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -23,9 +23,10 @@ export default function Hero() {
               animate={{ opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
             >
-        <h1>Freelance Full Stack Web Developer in Staffordshire</h1>
-        <h2>I Build Custom Websites Using WordPress, Laravel, Shopify, and Next.js</h2>
-<p>Hi, I'm Marcus, a freelance full stack webb developer specializing in custom website solutions tailored to meet the unique needs of your business.</p>
+       <h1>Freelance Full Stack Web Developer in Staffordshire</h1>
+        <h2>I Build <span class="bold">Custom Websites</span> Using WordPress, Laravel, Shopify, and Next.js</h2>
+<p>Hi, I&apos;m Marcus, a <span class="bold">freelance full stack webb developer</span> specializing in custom website solutions tailored to meet the unique needs of your business.</p>
+              
               
             </motion.div>
           </div>
